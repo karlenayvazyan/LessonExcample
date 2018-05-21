b54bfebbf6c70feaad9aa5b0c9586fbc27406f92
Karlen <karlen.ayvazyan@gmail.com>
2018-05-21 17:24:52 +0400

    Second lessons files

--- src/chapter1/arrays/ForAndWhile.java
+++ src/chapter1/arrays/ForAndWhile.java
@@ -3,18 +3,18 @@ package chapter1.arrays;
 public class ForAndWhile {
 
     public static void main(String[] args) {
-        if (30 % 2==1);
-        else(
-                )
+//        if (30 % 2==1);
+//        else(
+//                )
     }
 
     public static boolean isEven(int n) {
 //        return false; // this method return true if n is even false otherwise
-        if(n % 2 ==1){
+        if(n % 2 ==1){}
         return false;
-        else(n%==0 ){
-                return true;
-            }
-        }
+//        else(n%==0 ){
+//                return true;
+//            }
+//        }
     }
 }
--- src/homework/ArrayHomeWork.java
+++ src/homework/ArrayHomeWork.java
@@ -2,18 +2,32 @@ package homework;
 
 public class ArrayHomeWork {
 
+    public static void main(String[] args) {
+        int[] evenNumbers = getOddNumbers(10);
+        System.out.println(evenNumbers);
+    }
+
     /*
     create int array which will contains even numbers and return that array
-
      */
     public static int[] getEvenNumbers(int arrayLength) {
-        return new int[arrayLength];
+        int[] array = new int[arrayLength];
+        for (int i = 0; i < arrayLength * 2; i = i + 2) {
+            array[i] = i;
+        }
+        return array;
     }
 
+
     /*
-    create int array which contains odd numbers and return that array
+    create int array which will contains even numbers and return that array
      */
     public static int[] getOddNumbers(int arrayLength) {
-        return new int[arrayLength];
+        int[] array = new int[arrayLength];
+        for (int i = 1; i < arrayLength * 2 - 1; i = i + 2) {
+            array[i] = i;
+        }
+        return array;
     }
 }
+
